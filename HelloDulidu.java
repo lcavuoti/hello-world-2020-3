01716b9af91c01318f1677134b5256c80655c69d
lcavuoti <lcavuoti@clip.ch>
2020-10-23 08:02:57 +0200

    Text eingebaut

--- HelloDulidu.java
+++ HelloDulidu.java
@@ -2,8 +2,14 @@ public class HelloDulidu {
 
     private String hello = "Hello Guguseli";
     private String coolFeature = "My cool feature";
+    private String text = "FeatureA";
 
-    public HelloDulidu() {
+    public HelloDulidu(String text) {
+        this.text = text;
+        System.out.println("Hello Hellodulidudo");
+    }
+
+    public HelloDulidu(String text) {
         coolFeature = "cool";
     }
 
