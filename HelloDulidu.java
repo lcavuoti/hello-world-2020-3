79874ea7698b08ee2cebb13ced7e378f0cc51a18
lcavuoti <lcavuoti@clip.ch>
2020-10-30 10:28:28 +0100

    HelloDulidu geändert

--- HelloDulidu.java
+++ HelloDulidu.java
@@ -1,5 +1,6 @@
 public class HelloDulidu {
 
+    private int counter;
     private String hello = "Hello Guguseli";
     private String coolFeature = "My cool feature";
     private String text = "FeatureA";
@@ -21,4 +22,5 @@ public class HelloDulidu {
     public String getCoolFeature() {
         return this.coolFeature;
     }
+
 }
